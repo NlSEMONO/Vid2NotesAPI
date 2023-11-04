470a42f03cb4edf0371253c3fd4b540a695c42c0
Abhi K <abhigokul.k@gmail.com>
2023-11-04 19:58:47 -0400

    Made transcriptGenerator and transcriptProcesssing into functions for easier use, with better documentation Still working on transcriptProcesssing though

--- transcriptProcesssing.py
+++ transcriptProcesssing.py
@@ -1,5 +1,4 @@
 import requests
-
 import cohere
 import transcriptGenerator
 '''
@@ -7,69 +6,99 @@ Input: textChunks from transcriptGenerator.py
 Output: generatedSummary - String which contains the summary of the video fed into transcriptGenerator.py
 alternatively, you can use outputList - contains the same content as generatedSummary but separated by Cohere Request # 
 
+Note about Cohere API requests:
+    Each key has 5 requests each per minute. 
+    We cannot excede 4081 tokens, and each word is about 1-3 tokens
+    Therefore we cannot have more than 1360 words per request
+    Assuming a person speaks about 250 words per minute in the worst case, we can take 5-ish minutes of text for each request
+    Therefore, we can handle a video of 125 minutes?
+
 Sticking with Generate Function
 Used Prompts: Note: Square Brackets [] mean optional/extra
     What are the main points of this [into bullet points]:
     Summarize this into bullet Points
 
-Note: The Transcript Summary function on Cohere is very inaccurate
+Note: The Transcript Summary function on Cohere is very inaccurate, using Generate instead
 '''
 
 # initialize the Cohere Client with an API Key
 api_keys = ["5tF9d0IkQRph19apERAfxoudDUzmEnfyxo6pimfB", 
             "yRYJDINsAmmxz7y00xUfeAHAaUqjAO1c7XXLXzhv",
             "ivsDVDocsH8LnNhQ7b21PZURol6yr5x8UCVRwGdh"]
-co = cohere.Client(api_keys[0])
 
-transcript = ""
-promptList = ["What are the main points of this in bullet points:", 
-              "Summarize this into bullet points:"]
 
-"""
-For each sub-string, we need a prompt
-co.generate Responses:
-200 = ok
-400 = bad request
-498 = Blocked Input or Output
-500 = Internal Server Errors
-"""
-outputList = []
-count = 0
-api_index = 0
-maxCount = 5
+def process_transcript(text_chunks):
+    co = cohere.Client(api_keys[0])
+    
+    promptList = ["What are the main points of this in bullet points:", 
+                "Summarize this into bullet points:"]
 
-for text in transcriptGenerator.textChunks:
-    if(count < maxCount):
-        cohere_response = co.generate(
-                model='command-nightly',
-                prompt= (promptList[0] + "\n\"" + text + "\""),
-                max_tokens=4050,
-                temperature=0.5)
-        #if(cohere_response.response == 200):
-        outputList = list(cohere_response.generations[0].text.split("\n"))
-        outputList.remove(outputList[-1])
-        outputList.remove(outputList[0])
-        count += 1
-        print("count")
-    else:
-        count = 0
-        api_index += 1
-        if(api_index >= len(api_keys)):
-            print("BREAK ASAP")
-            break
+    """
+    For each sub-string, we need a prompt
+    co.generate Responses:
+    200 = ok
+    400 = bad request
+    498 = Blocked Input or Output
+    500 = Internal Server Errors
+    """
+    outputList = []
+    count = 0
+    api_index = 0
+    maxCount = 4
+    response = 0
+
+    for text in text_chunks:
+        if(count < maxCount):
+            cohere_response = co.generate(
+                    model='command-nightly',
+                    prompt= (promptList[0] + "\n\"" + text + "\""),
+                    max_tokens=4050,
+                    temperature=0.5,
+                    truncate="END")
+            
+            
+            
+            outputList = list(cohere_response.generations[0].text.split("\n"))
+            outputList.remove(outputList[-1])
+            outputList.remove(outputList[0])
+            count += 1
+            #print("count")
+            
+            
+            
+            
+            
         else:
-            co = cohere.Client(api_keys[api_index])
-    #print(str(stuff))
-    #else:
-        #    print("ERROR")
+            count = 0
+            api_index += 1
+            if(api_index >= len(api_keys)):
+                print("BREAK ASAP")
+                break
+            else:
+                co = cohere.Client(api_keys[api_index])
+        #print(str(stuff))
+        #else:
+            #print("ERROR")
 
-#print(outputList)
+    #print(outputList)
 
-#print(str(outputList))
+    #print(str(outputList))
+    
+    generatedOutput = ""
+    for k in outputList:
+        generatedOutput += k
+        generatedOutput += "\n"
+    
+    """
+    Debug: just output the generated summary
+    """
+    #print(generatedOutput)
+    
+    return generatedOutput
 
-generatedOutput = ""
-for k in outputList:
-    generatedOutput += k
-    generatedOutput += "\n"
+"""
+Debug:
+"""
 
-print(generatedOutput)
+testOut = process_transcript(transcriptGenerator.generate_transcript(transcriptGenerator.test_URL))
+print(testOut)
