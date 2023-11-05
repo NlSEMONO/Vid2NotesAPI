8899f1adf2d32d395d0e13517c4549d4a16f1800
NlSEMONO <fmadarang2542@hotmail.com>
2023-11-05 08:54:43 -0500

    switch get-notes route back to what it should be.

--- server/notemaker/urls.py
+++ server/notemaker/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('get-notes', views.get_notes),
-    path('get-notes', views.temp),
+    path('get-notes', views.get_notes),
+    # path('get-notes', views.temp),
     path('get-cards', views.fill_in_the_blanks)
 ]
